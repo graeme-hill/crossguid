2e546333372536beb93a2ce6c56e8c0d07b75532
Lars Melchior <TheLartians@users.noreply.github.com>
2021-01-25 22:17:32 +0100

    try another FindLibuuid.cmake

--- cmake/FindLibuuid.cmake
+++ cmake/FindLibuuid.cmake
@@ -1,43 +1,85 @@
-find_package(PkgConfig)
+# Distributed under the OSI-approved BSD 3-Clause License.  See accompanying
+# file Copyright.txt or https://cmake.org/licensing for details.
 
-pkg_check_modules(PKG_LIBUUID QUIET uuid)
+#[=======================================================================[.rst:
+FindLibUUID
+------------
 
-set(LIBUUID_DEFINITIONS ${PKG_LIBUUID_CFLAGS_OTHER})
-set(LIBUUID_VERSION ${PKG_LIBUUID_VERSION})
+Find LibUUID include directory and library.
 
-find_path(LIBUUID_INCLUDE_DIR
-	NAMES uuid/uuid.h
-	HINTS ${PKG_LIBUUID_INCLUDE_DIRS}
-)
-find_library(LIBUUID_LIBRARY
-	NAMES uuid
-	HINTS ${PKG_LIBUUID_LIBRARY_DIRS}
-)
+Imported Targets
+^^^^^^^^^^^^^^^^
 
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(LibUUID
-	FOUND_VAR
-		LIBUUID_FOUND
-	REQUIRED_VARS
-		LIBUUID_LIBRARY
-		LIBUUID_INCLUDE_DIR
-	VERSION_VAR
-		LIBUUID_VERSION
-)
+An :ref:`imported target <Imported targets>` named
+``LibUUID::LibUUID`` is provided if LibUUID has been found.
 
-if(LIBUUID_FOUND AND NOT TARGET LibUUID::UUID)
-	add_library(LibUUID::UUID UNKNOWN IMPORTED)
-	set_target_properties(LibUUID::UUID PROPERTIES
-		IMPORTED_LOCATION "${LIBUUID_LIBRARY}"
-		INTERFACE_COMPILE_OPTIONS "${LIBUUID_DEFINITIONS}"
-		INTERFACE_INCLUDE_DIRECTORIES "${LIBUUID_INCLUDE_DIR}"
-	)
+Result Variables
+^^^^^^^^^^^^^^^^
+
+This module defines the following variables:
+
+``LibUUID_FOUND``
+  True if LibUUID was found, false otherwise.
+``LibUUID_INCLUDE_DIRS``
+  Include directories needed to include LibUUID headers.
+``LibUUID_LIBRARIES``
+  Libraries needed to link to LibUUID.
+
+Cache Variables
+^^^^^^^^^^^^^^^
+
+This module uses the following cache variables:
+
+``LibUUID_LIBRARY``
+  The location of the LibUUID library file.
+``LibUUID_INCLUDE_DIR``
+  The location of the LibUUID include directory containing ``uuid/uuid.h``.
+
+The cache variables should not be used by project code.
+They may be set by end users to point at LibUUID components.
+#]=======================================================================]
+
+#-----------------------------------------------------------------------------
+if(CYGWIN)
+  # Note: on current version of Cygwin, linking to libuuid.dll.a doesn't
+  #       import the right symbols sometimes. Fix this by linking directly
+  #       to the DLL that provides the symbols, instead.
+  set(old_suffixes ${CMAKE_FIND_LIBRARY_SUFFIXES})
+  set(CMAKE_FIND_LIBRARY_SUFFIXES .dll)
+  find_library(LibUUID_LIBRARY
+    NAMES cyguuid-1.dll
+    )
+  set(CMAKE_FIND_LIBRARY_SUFFIXES ${old_suffixes})
+else()
+  find_library(LibUUID_LIBRARY
+    NAMES uuid
+    )
 endif()
+mark_as_advanced(LibUUID_LIBRARY)
 
-mark_as_advanced(LIBUUID_INCLUDE_DIR LIBUUID_LIBRARY)
+find_path(LibUUID_INCLUDE_DIR
+  NAMES uuid/uuid.h
+  )
+mark_as_advanced(LibUUID_INCLUDE_DIR)
 
-include(FeatureSummary)
-set_package_properties(LIBUUID PROPERTIES
-	URL "http://www.kernel.org/pub/linux/utils/util-linux/"
-	DESCRIPTION "uuid library in util-linux"
-)
+#-----------------------------------------------------------------------------
+include(FindPackageHandleStandardArgs)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(LibUUID
+  FOUND_VAR LibUUID_FOUND
+  REQUIRED_VARS LibUUID_LIBRARY LibUUID_INCLUDE_DIR
+  )
+set(LIBUUID_FOUND ${LibUUID_FOUND})
+
+#-----------------------------------------------------------------------------
+# Provide documented result variables and targets.
+if(LibUUID_FOUND)
+  set(LibUUID_INCLUDE_DIRS ${LibUUID_INCLUDE_DIR})
+  set(LibUUID_LIBRARIES ${LibUUID_LIBRARY})
+  if(NOT TARGET LibUUID::LibUUID)
+    add_library(LibUUID::LibUUID UNKNOWN IMPORTED)
+    set_target_properties(LibUUID::LibUUID PROPERTIES
+      IMPORTED_LOCATION "${LibUUID_LIBRARY}"
+      INTERFACE_INCLUDE_DIRECTORIES "${LibUUID_INCLUDE_DIRS}"
+      )
+  endif()
+endif()
